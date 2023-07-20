bd75840f57eef525195e0d237a5917be7d354d8e
huseynovth <tahireh@code.edu.az>
2023-07-21 03:30:31 +0400

    Auto Mapper is done

---
+++ src/Infrastructure/Fiorella.Persistence/MapperProfiles/CategoryProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Fiorella.Aplication.Abstraction.Services;
+using Fiorella.Aplication.DTOs.CategoryDTOs;
+using Fiorella.Domain.Entites;
+
+namespace Fiorella.Persistence.MapperProfiles;
+
+public class CategoryProfile:Profile
+{
+    public CategoryProfile()
+    {
+        CreateMap<Category, CategoryCreateDto>().ReverseMap();
+        CreateMap<Category, CategoryGetDto>().ReverseMap();
+        CreateMap<Category, CategoryUpdateDto>().ReverseMap();
+    }
+}
