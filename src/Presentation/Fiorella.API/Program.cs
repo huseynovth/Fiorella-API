137bf56f244bd7728773200e6fd6a7b402c47cde
huseynovth <tahireh@code.edu.az>
2023-07-30 02:08:29 +0400

    Seed is done

--- src/Presentation/Fiorella.API/Program.cs
+++ src/Presentation/Fiorella.API/Program.cs
@@ -18,34 +18,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddPresistenceService();
+builder.Services.AddScoped<AppDbContextInitializer>();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
 
-
-
-//builder.Services.AddFluentValidationAutoValidation();
-//builder.Services.AddFluentValidationClientsideAdapters();
-//builder.Services.AddValidatorsFromAssemblyContaining<CategoryCreateDtoValidator>();   
-//builder.Services.AddAutoMapper(typeof(CategoryProfile).Assembly);
-
-//builder.Services.AddDbContext<AppDbContext>(options =>
-//{
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
-//});
-//builder.Services.AddScoped<ICategoryReadRepository, CategoryReadRepository>();
-//builder.Services.AddScoped<ICategoryWriteRepository, CategoryWriteRepository>();
-
-//builder.Services.AddScoped<ICategoryService, CategoryService>();
-
-
-
+using (var scope = app.Services.CreateScope())
+{
+    var instance = scope.ServiceProvider.GetRequiredService<AppDbContextInitializer>();
+    await instance.InitializerAsync();
+    await instance.RoleSeedAsync();
+    await instance.UserSeedAsync();
+}
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 
 if (app.Environment.IsDevelopment())
 {
